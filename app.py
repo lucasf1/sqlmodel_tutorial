1087a15bf9dbbd5bdffe308936fca61544c5ed89
Lucas Farias <lucasf1@gmail.com>
2025-05-19 06:36:28 -0300

    Usando o Where

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from sqlmodel import Field, Session, SQLModel, col, create_engine, select
 
 
 class Hero(SQLModel, table=True):
@@ -19,29 +19,38 @@ def create_db_and_tables():
 
 
 def create_heroes():
-    hero_1 = Hero(name='Deadpond', secret_name='Dive Wilson')
-    hero_2 = Hero(name='Spider-Boy', secret_name='Pedro Parqueador')
-    hero_3 = Hero(name='Rusty-Man', secret_name='Tommy Sharp', age=48)
+    hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
+    hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
+    hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
+    hero_4 = Hero(name="Tarantula", secret_name="Natalia Roman-on", age=32)
+    hero_5 = Hero(name="Black Lion", secret_name="Trevor Challa", age=35)
+    hero_6 = Hero(name="Dr. Weird", secret_name="Steve Weird", age=36)
+    hero_7 = Hero(name="Captain North America", secret_name="Esteban Rogelios", age=93)
 
     with Session(engine) as session:
         session.add(hero_1)
         session.add(hero_2)
         session.add(hero_3)
+        session.add(hero_4)
+        session.add(hero_5)
+        session.add(hero_6)
+        session.add(hero_7)
 
         session.commit()
 
-        
+
 def select_heroes():
     with Session(engine) as session:
-        statement = select(Hero)
-        # results = session.exec(statement)
-        # for hero in results:
-        #     print(hero)
+        # statement = select(Hero).where(Hero.age >= 35).where(Hero.age < 40)
+        statement = select(Hero).where(col(Hero.age) >= 35)
+        results = session.exec(statement)
+        for hero in results:
+            print(hero)
 
         # heroes = results.all()
-        
-        heroes = session.exec(statement).all()
-        print(heroes)
+
+        # heroes = session.exec(statement).all()
+        # print(heroes)
 
 
 def main():
