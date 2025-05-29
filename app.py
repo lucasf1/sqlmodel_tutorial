424a6d1627a13e617f6b09a7ceb53f10218081dc
Lucas Farias <lucasf1@gmail.com>
2025-05-29 20:56:54 -0300

    Atualizando um valor em uma tabela - update

--- app.py
+++ app.py
@@ -42,16 +42,45 @@ def create_heroes():
 
 def select_heroes():
     with Session(engine) as session:
-        statement = select(Hero).where(col(Hero.age) > 32).offset(1).limit(2)
+        statement = select(Hero).where(col(Hero.age) <= 35)
         results = session.exec(statement)
         heroes = results.all()
         print(heroes)
+
+
+def update_heroes():
+    with Session(engine) as session:
+        statement = select(Hero).where(Hero.name == "Spider-Boy")  
+        results = session.exec(statement)  
+        hero_1 = results.one()  
+        print("Hero 1:", hero_1)  
+
+        statement = select(Hero).where(Hero.name == "Captain North America")  
+        results = session.exec(statement)  
+        hero_2 = results.one()  
+        print("Hero 2:", hero_2)  
+
+        hero_1.age = 16  
+        hero_1.name = "Spider-Youngster"  
+        session.add(hero_1)  
+
+        hero_2.name = "Captain North America Except Canada"  
+        hero_2.age = 110  
+        session.add(hero_2)  
+
+        session.commit()  
+        session.refresh(hero_1)  
+        session.refresh(hero_2)  
+
+        print("Updated hero 1:", hero_1)  
+        print("Updated hero 2:", hero_2)  
 
 
 def main():
     create_db_and_tables()
     create_heroes()
     select_heroes()
+    update_heroes()
 
 
 if __name__ == '__main__':
